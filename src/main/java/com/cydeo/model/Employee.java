9f06a5f8f6875dd84894292beb01f84de4cc28ea
madiyard <icloudka@gmail.com>
2022-10-19 13:18:18 -0700

    Lab completed

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,6 +1,7 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 public class Employee {
 
     private String firstName, lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email, password, address, address2, city, state, zipCode;
 
